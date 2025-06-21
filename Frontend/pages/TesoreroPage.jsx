6b9ccfa352eac884c422714e22bc290fb91596b9
¨Nicolas <nicolasappas@outlook.com>
2025-06-21 02:09:06 -0300

    agregado footer y header a tesoreros

--- Frontend/pages/TesoreroPage.jsx
+++ Frontend/pages/TesoreroPage.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import MainTesorero from '../component/tesorero/MainTesorero.jsx'
+import React from 'react';
+import MainTesorero from '../component/tesorero/MainTesorero.jsx';
+import Header from '../component/componentGenerales/Header';
+import Footer from '../component/componentGenerales/Footer';
 const TesoreroPage = () => {
-  return (
-    <div>
-        <MainTesorero />
-    </div>
-  )
-}
+	return (
+		<div>
+			<Header />
+			<MainTesorero />
+			<Footer />
+		</div>
+	);
+};
 
-export default TesoreroPage
+export default TesoreroPage;
